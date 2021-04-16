8171a0d5be3b7486f01db6799ef082c026e03de3
wu_qian <wu_qian@founder.com.cn>
2021-04-16 14:57:07 +0800

    上传组件

---
+++ src/components/ReactMarquee.js
@@ -0,0 +1,16 @@
+/**
+ * Description: 组件
+ * @date: 2021/4/16 2:28 下午
+ * @author: wuqian
+ */
+import React from 'react';
+
+const ReactMarquee = () => {
+  return (
+    <div>
+      ReactMarquee
+    </div>
+  );
+};
+
+export default ReactMarquee;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+/**
+ * Description: 入口文件
+ * @date: 2021/4/16 2:27 下午
+ * @author: wuqian
+ */
+import ReactMarquee from "./components/ReactMarquee";
+
+module.exports = {
+  ReactMarquee
+}
